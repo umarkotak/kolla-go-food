344f3d0749357248d0806c4d3a092c3f26241f52
umarkotak <umarkotak@yahoo.co.id>
2017-10-23 16:01:06 +0700

    5th commit, selecting from database

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def hello
     @time = Time.now
+
+    # @users = User.where(username: 'umarkotak')
+    @users = User.all
   end
 
   def goodbye
--- db/migrate/20171023073504_create_users.rb
+++ db/migrate/20171023073504_create_users.rb
@@ -1,6 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :username, null: false
+      t.string :full_name 
 
       t.timestamps
     end
