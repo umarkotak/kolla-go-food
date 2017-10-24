9fd4b0c04fba4f3d230558ec0c4136c82de01f13
umarkotak <umarkotak@yahoo.co.id>
2017-10-24 17:34:28 +0700

    12th commit, on exercise, add validation on buyers input

--- app/models/buyer.rb
+++ app/models/buyer.rb
@@ -1,2 +1,8 @@
 class Buyer < ApplicationRecord
+  validates :name, :email, :phone, :address, presence: true
+  validates :phone, numericality: true, length: { is: 12 }
+  # validates :name, uniqueness: true
+  validates :email, format: {
+    with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  }
 end
