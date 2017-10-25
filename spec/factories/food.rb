c1775c578cf99cfd8a2f4415354ff9b96ea6ee3e
umarkotak <umarkotak@yahoo.co.id>
2017-10-25 15:58:58 +0700

    20th commit, on exercise, add some factory girl data

--- spec/factories/food.rb
+++ spec/factories/food.rb
@@ -2,10 +2,16 @@ FactoryGirl.define do
   factory :food do
     name {Faker::Food::dish}
     description {Faker::Food::ingredient}
-    price {Faker::Number::number}
+    price {Faker::Number::number(3)}
 
     # name "Nasi Uduk"
     # description "Nasi paling enak aja"
     # price 5000
+  end
+
+  factory :invalid_food, parent: :food do
+    name nil
+    description nil
+    price 10000
   end
 end
