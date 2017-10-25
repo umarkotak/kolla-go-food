95509e5761b464906ca10be91185eda017e9e3ac
umarkotak <umarkotak@yahoo.co.id>
2017-10-25 10:14:50 +0700

    13th commit, add test validation

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,6 @@
 class Food < ApplicationRecord
-  validates :name,:description, presence: true
+  validates :name, :description, presence: true
+
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :name, uniqueness: true
   validates :image_url, allow_blank: true, format: {
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 describe Food do
-  it "is valid with a name and description"
+  it "is valid with a name and description" do
+    food = Food.new(
+      name: 'Nasi Uduk',
+      description: 'Betawi style steamed rice cooked in coconut milk.',
+      price: 5000
+    )
+    expect(food).to be_valid
+  end
+
+  it "is invalid without a name" do
+    food = Food.new(
+      name: nil,
+      description: 'halo',
+      price: 5000
+    )
+    food.valid?
+    expect(food.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without a description" do
+    food = Food.new(
+      name: 'Nasi Jamblank',
+      description: nil,
+      price: 5000
+    )
+    food.valid?
+    expect(food.errors[:description]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate name" do
+    food1 = Food.create(
+      name: 'Nasi Uduk',
+      description: 'Nasik enak',
+      price: 5000
+    )
+
+    food2 = Food.new(
+      name: 'Nasi Uduk',
+      description: 'Nasik enak',
+      price: 6000
+    )
+
+    food2.valid?
+    expect(food2.errors[:name]).to include('has already been taken')
+  end
+
 end
