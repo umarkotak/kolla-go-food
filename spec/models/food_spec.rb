881a42f1f198d229802b91824d7bbe5e699b407f
umarkotak <umarkotak@yahoo.co.id>
2017-10-25 13:26:39 +0700

    18th commit, on exercise, using rails_helper for easier factory girl

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -205,6 +205,30 @@ describe Food do
   it "is invalid with a duplicate name" do
     food1 = FactoryGirl.create(:food, name: 'Nasi Uduk')
     food2 = FactoryGirl.build(:food, name: 'Nasi Uduk')
+
+    food2.valid?
+    expect(food2.errors[:name]).to include('has already been taken')
+  end
+
+  it "has a valid factory" do
+    expect(build(:food)).to be_valid
+  end
+
+  it "is invalid without a name" do
+    food = build(:food, name: nil)
+    food.valid?
+    expect(food.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without a description" do
+    food = build(:food, description: nil)
+    food.valid?
+    expect(food.errors[:description]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate name" do
+    food1 = create(:food, name: 'Nasi Uduk')
+    food2 = build(:food, name: 'Nasi Uduk')
     
     food2.valid?
     expect(food2.errors[:name]).to include('has already been taken')
