57dba066c8d042d2e6bf90b7ca585820d2d8cb84
umarkotak <umarkotak@yahoo.co.id>
2017-10-25 13:18:41 +0700

    17th commit, on exercise, using FactoryGirl

---
+++ spec/factories/food.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :food do
+    name "Nasi Uduk"
+    description "Nasi paling enak aja"
+    price 5000
+  end
+end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -185,5 +185,29 @@ describe Food do
       end
     end
   end
+  
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:food)).to be_valid
+  end
+
+  it "is invalid without a name" do
+    food = FactoryGirl.build(:food, name: nil)
+    food.valid?
+    expect(food.errors[:name]).to include("can't be blank")
+  end
+
+  it "is invalid without a description" do
+    food = FactoryGirl.build(:food, description: nil)
+    food.valid?
+    expect(food.errors[:description]).to include("can't be blank")
+  end
+
+  it "is invalid with a duplicate name" do
+    food1 = FactoryGirl.create(:food, name: 'Nasi Uduk')
+    food2 = FactoryGirl.build(:food, name: 'Nasi Uduk')
+    
+    food2.valid?
+    expect(food2.errors[:name]).to include('has already been taken')
+  end
 
 end
