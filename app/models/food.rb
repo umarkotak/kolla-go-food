4bb66d055d8a1f1b5a6f78911a8bd57f01f1cbcc
umarkotak <umarkotak@yahoo.co.id>
2017-10-26 16:46:02 +0700

    27th commit, on issue 3, add more test unit on item line

--- app/models/food.rb
+++ app/models/food.rb
@@ -11,4 +11,16 @@ class Food < ApplicationRecord
   def self.by_letter(letter)
     where("name LIKE ?", "#{letter}%").order(:name)
   end
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line Items present')
+        throw :abort
+      end
+    end
 end
