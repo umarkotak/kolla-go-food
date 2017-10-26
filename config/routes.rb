fa57e6f5b599d7e59aae2c9c35734c3e0b6b2d27
umarkotak <umarkotak@yahoo.co.id>
2017-10-26 15:41:06 +0700

    25th commit, on issue 3, done with store menu view with new routes, and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  root 'store#index', as: 'store_index'
+
   get 'store/index'
 
   resources :buyers
   resources :foods
+
+
+
   get 'home/hello'
   get 'home/goodbye'
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/controllers/store_controller_spec.rb
+++ spec/controllers/store_controller_spec.rb
@@ -7,6 +7,13 @@ RSpec.describe StoreController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it "returns a list of foods, ordered by name" do
+      nasi_uduk = create(:food, name: "Nasi Uduk")
+      kerak_telor = create(:food, name: "Kerak Telor")
+      get :index
+      expect(assigns(:foods)).to eq([kerak_telor, nasi_uduk])
+    end
   end
 
 end
