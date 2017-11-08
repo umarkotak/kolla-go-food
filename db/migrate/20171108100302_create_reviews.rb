182634a2c8d68ad3a36680505e90ee7dc6f30871
umarkotak <umarkotak@yahoo.co.id>
2017-11-08 17:33:00 +0700

    after add migration

--- db/migrate/20171108100302_create_reviews.rb
+++ db/migrate/20171108100302_create_reviews.rb
@@ -1,6 +1,11 @@
 class CreateReviews < ActiveRecord::Migration[5.1]
   def change
     create_table :reviews do |t|
+      t.string :name
+      t.string :title
+      t.text :description
+
+      t.references :reviewable, polymorphic: true, index: true
 
       t.timestamps
     end
