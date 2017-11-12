e6c19be2c919ca628f08a4335a8ffb2d2647302b
umarkotak <umarkotak@yahoo.co.id>
2017-11-13 06:16:40 +0700

    check

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,7 @@ class DashboardController < ApplicationController
     @total_price_of_order_every_date = Order.group(:created_at).sum(:total)
     @number_of_order_for_each_food = LineItem.joins(:food).group("foods.name").sum("line_items.quantity")
     @total_price_of_order_for_each_food = LineItem.joins(:food, :order).group("foods.name").sum("foods.price * line_items.quantity")
-    @number_of_order_from_each_restaurant
-    @total_price_of_order_from_each_restaurant
+    @number_of_order_from_each_restaurant = LineItem.joins(:order, food: :restaurant).group("restaurants.name").count("orders.id")
+    @total_price_of_order_from_each_restaurant = LineItem.joins(:order, food: :restaurant).group("restaurants.name").sum("foods.price * line_items.quantity")
   end
 end
